67c2847394cdadb7f5bdb82112d7671d27d086ca
Leonardo <leonit@alu.edu.gva.es>
2025-01-23 20:40:12 +0100

    Ejercicios 1, 2 y 3

---
+++ src/Ejercicio2/AireAcondicionado.java
@@ -0,0 +1,56 @@
+package Ejercicio2;
+
+public class AireAcondicionado {
+    private String nombre;
+    final private int maxTemp;
+    final private int minTemp;
+    private int temperatura;
+
+    //Constructor
+    public AireAcondicionado(String nombre, int maxTemp, int minTemp, int temperatura) {
+        this.nombre = nombre;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.temperatura = temperatura;
+    }
+
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+
+    public int getMinTemp() {
+        return minTemp;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    //Métodos
+    public void subirTemperatura() {
+        if (temperatura < maxTemp) temperatura++;
+    }
+
+    public void bajarTemperatura() {
+        if (temperatura > minTemp) temperatura--;
+    }
+
+    @Override
+    public String toString() {
+        return "La temperatura actual del aire acondicionado " + this.nombre + " es " + this.temperatura;
+    }
+}
