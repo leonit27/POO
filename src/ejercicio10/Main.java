e970d463f084725498f7b47d13932a3ba7baf4b2
Leonardo <leonit@alu.edu.gva.es>
2025-02-10 19:36:09 +0100

    Ejercicio 10 definitivamente acabado

--- src/ejercicio10/Main.java
+++ src/ejercicio10/Main.java
@@ -5,57 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         MaquinaExpendedora maquinaExpendedora = new MaquinaExpendedora();
-        double[] preciosTickets = {1.20, 2.30, 3.80};
-        double[] monedas = {0.10, 0.20, 0.50, 1, 2};
-        String continuar;
+        Scanner s = new Scanner(System.in);
+        String empezar;
         System.out.println("MÁQUINA EXPENDEDORA");
 
-        do {
-            System.out.println("Ticket 1: " + preciosTickets[0] + "€\nTicket 2: " + preciosTickets[1] + "€\nTicket 3: " + preciosTickets[2] + "€");
+        System.out.print("Deseas sacar un ticket? (S/N): ");
+        empezar = s.next();
 
-            Scanner s = new Scanner(System.in);
-
-            int ticketDeseado;
-            do {
-                System.out.print("Introduce el número del ticket que deseas sacar: ");
-                ticketDeseado = s.nextInt();
-
-                if (ticketDeseado < 1 || ticketDeseado > 3) System.out.println("El ticket introducido es incorrecto");
-            } while (ticketDeseado < 1 || ticketDeseado > 3);
-
-            double monedaIntroducida;
-            do {
-                System.out.print("Introduce las monedas (en euros), para acabar pulsa 0: ");
-                monedaIntroducida = s.nextDouble();
-                boolean monedaValida = false;
-                for (double moneda : monedas) {
-                    if (monedaIntroducida == moneda) {
-                        maquinaExpendedora.addDinero(monedaIntroducida);
-                        monedaValida = true;
-                        break;
-                    }
-                }
-                if (!monedaValida && monedaIntroducida != 0) System.out.println("Moneda introducida incorrecta");
-            } while (monedaIntroducida != 0);
-
-            if (maquinaExpendedora.getDinero() >= preciosTickets[ticketDeseado - 1]) {
-                double dineroDevolucion = maquinaExpendedora.getDinero() - preciosTickets[ticketDeseado - 1];
-                if (dineroDevolucion > maquinaExpendedora.getCajon()) {
-                    System.out.println("Introduzca el importe exacto, no hay dinero para devolver en el cajón");
-                } else {
-                    System.out.println("El ticket ha sido sacado correctamente, dinero a devolver " + dineroDevolucion + "€");
-                    String devolucion = maquinaExpendedora.devolverDinero(dineroDevolucion);
-                    System.out.println(devolucion);
-                    maquinaExpendedora.addCajon(preciosTickets[ticketDeseado - 1]);
-                }
-            } else {
-                System.out.println("Dinero insuficiente para sacar el ticket.");
-            }
-
-            maquinaExpendedora.setDinero(0);
-
-            System.out.print("Deseas continuar comprando? (S/N): ");
-            continuar = s.next();
-        } while (continuar.equalsIgnoreCase("S"));
+        if (empezar.equalsIgnoreCase("S")) maquinaExpendedora.sacarTickets();
     }
 }
--- src/ejercicio10/MaquinaExpendedora.java
+++ src/ejercicio10/MaquinaExpendedora.java
@@ -1,6 +1,9 @@
 package ejercicio10;
 
+import java.util.Scanner;
+
 public class MaquinaExpendedora {
+    //Creamos las variables
     private double dinero;
     private double cajon;
     private double[] monedas = {2, 1, 0.50, 0.20, 0.10};
@@ -13,6 +16,7 @@ public class MaquinaExpendedora {
         this.cajon += dinero;
     }
 
+
     public String devolverDinero(double dineroDevolucion) {
         StringBuilder devolucion = new StringBuilder();
         for (double m : monedas) {
@@ -22,12 +26,62 @@ public class MaquinaExpendedora {
                 cajon -= m;
             }
         }
-        if (dineroDevolucion > 0) {
-            return "No hay suficiente dinero en el cajón para devolver el cambio exacto.";
-        }
         return devolucion.toString();
     }
+    
+    public void sacarTickets() {
+        Scanner s = new Scanner(System.in);
+        double[] preciosTickets = {1.20, 2.30, 3.80};
+        double[] monedas = {0.10, 0.20, 0.50, 1, 2};
+        String continuar;
+        do {
+            System.out.println("Ticket 1: " + preciosTickets[0] + "€\nTicket 2: " + preciosTickets[1] + "€\nTicket 3: " + preciosTickets[2] + "€");
 
+            int ticketDeseado;
+            do {
+                System.out.print("Introduce el número del ticket que deseas sacar: ");
+                ticketDeseado = s.nextInt();
+
+                if (ticketDeseado < 1 || ticketDeseado > 3) System.out.println("El ticket introducido es incorrecto");
+            } while (ticketDeseado < 1 || ticketDeseado > 3);
+
+            double monedaIntroducida;
+            do {
+                System.out.print("Introduce las monedas (en euros), para acabar pulsa 0: ");
+                monedaIntroducida = s.nextDouble();
+                boolean monedaValida = false;
+                for (double moneda : monedas) {
+                    if (monedaIntroducida == moneda) {
+                        addDinero(monedaIntroducida);
+                        monedaValida = true;
+                        break;
+                    }
+                }
+                if (!monedaValida && monedaIntroducida != 0) System.out.println("Moneda introducida incorrecta");
+            } while (monedaIntroducida != 0);
+
+            if (getDinero() >= preciosTickets[ticketDeseado - 1]) {
+                double dineroDevolucion = getDinero() - preciosTickets[ticketDeseado - 1];
+                if (dineroDevolucion > getCajon()) {
+                    System.out.println("Introduzca el importe exacto, no hay dinero para devolver en el cajón");
+                } else {
+                    System.out.println("El ticket ha sido sacado correctamente, dinero a devolver " + dineroDevolucion + "€");
+                    String devolucion = devolverDinero(dineroDevolucion);
+                    System.out.println(devolucion);
+                    addCajon(preciosTickets[ticketDeseado - 1]);
+                }
+            } else {
+                System.out.println("Dinero insuficiente para sacar el ticket.");
+            }
+
+            setDinero(0);
+
+            System.out.print("Deseas continuar comprando? (S/N): ");
+            continuar = s.next();
+        } while (continuar.equalsIgnoreCase("S"));
+    }
+
+    //Getters
     public double getDinero() {
         return dinero;
     }
@@ -36,6 +90,7 @@ public class MaquinaExpendedora {
         return cajon;
     }
 
+    //Setters
     public void setDinero(double dinero) {
         this.dinero = dinero;
     }
